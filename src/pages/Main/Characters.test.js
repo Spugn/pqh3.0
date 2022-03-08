151fd595b5125ac081ba3aa08ceaacd9d06adc1a
S'pugn <Expugn@users.noreply.github.com>
2022-03-07 20:40:09 -0800

    Update Characters.test.js

--- src/pages/Main/Characters.test.js
+++ src/pages/Main/Characters.test.js
@@ -8,7 +8,11 @@ const data = {
         data: _data.character,
     },
 };
-const userState = {};
+const userState = {
+    settings: {
+        region: "JP",
+    },
+};
 const userDispatch = () => {};
 const hidden = false;
 
