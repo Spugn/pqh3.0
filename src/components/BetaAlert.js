ab937a95d7aae36115076bf5930220fd25a842f3
S'pugn <Expugn@users.noreply.github.com>
2022-10-08 09:48:59 -0700

    Fix BetaAlert
    Forgot to add a new line break lol.

--- src/components/BetaAlert.js
+++ src/components/BetaAlert.js
@@ -12,7 +12,7 @@ export default function BetaAlert() {
             - <strong>There may also be uncaught errors or bugs.</strong><br />
             - Saved user data may also be incompatible with later versions.<br />
             - Please visit <a href="https://github.com/Spugn/priconne-quest-helper" className="font-bold">
-                https://github.com/Spugn/priconne-quest-helper</a> to follow development.
+                https://github.com/Spugn/priconne-quest-helper</a> to follow development.<br />
             - UPDATE (October 8, 2022): <a href="https://github.com/Spugn/priconne-quest-helper#development-update-october-8-2022" className="font-bold">development-update-october-8-2022</a>
         </Alert>
     );
