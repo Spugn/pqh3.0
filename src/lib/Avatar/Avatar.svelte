2b137e98367bf9113b6c791008896e5c3db97828
S'pugn <Expugn@users.noreply.github.com>
2022-12-21 01:29:59 -0800

    Update Avatar.svelte
    - Convert to use TypeScript
    - also trying to trigger gh-pages deployment

--- src/lib/Avatar/Avatar.svelte
+++ src/lib/Avatar/Avatar.svelte
@@ -3,24 +3,13 @@
     import "./avatar.css";
 </script>
 
-<script>
-    /** @type {string} */
-    export let avatar = "";
-
-    /** @type {string} */
-    export let type = "";
-
-    /** @type {boolean} */
-    export let small = false;
-
-    /** @type {boolean} */
-    export let large = false;
+<script lang="ts">
+    export let avatar : string = "";
+    export let type : string = "";
+    export let small : boolean = false;
+    export let large : boolean = false;
 </script>
 
 <span class="avatar" class:avatar-small={small} class:avatar-large={large}>
     <Image img={avatar} type={type} props={{class: "avatar-image"}} />
 </span>
-
-<style>
-
-</style>
